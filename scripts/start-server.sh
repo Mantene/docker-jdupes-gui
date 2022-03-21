73e31b696ac0a91eb1103cbcf8d39611f397dba7
Mantene <Mantene@users.noreply.github.com>
2022-03-21 11:48:34 -0400

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,70 +1,27 @@
 #!/bin/bash
 export DISPLAY=:99
+export XDG_RUNTIME_DIR=/tmp/xdg
+export LANGUAGE="$LOCALE_USR"
+export LANG="$LOCALE_USR"
 export XAUTHORITY=${DATA_DIR}/.Xauthority
-CUR_V="$(${DATA_DIR}/firefox --version 2>/dev/null | cut -d ' ' -f3)"
-if [ "${FIREFOX_V}" == "latest" ]; then
-	LAT_V="$(wget -qO- https://github.com/ich777/versions/raw/master/Firefox | grep LATEST | cut -d '=' -f2)"
-	sleep 2
-	FIREFOX_V="${LAT_V}"
-	if [ -z "$LAT_V" ]; then
-		if [ ! -z "$CUR_V" ]; then
-			echo "---Can't get latest version of Firefox falling back to v$CUR_V---"
-			LAT_V="$CUR_V"
-		else
-			echo "---Something went wrong, can't get latest version of Firefox, putting container into sleep mode---"
-			sleep infinity
-		fi
-	fi
-fi
 
-rm ${DATA_DIR}/Firefox-*.tar.bz2 2>/dev/null
-
-echo "---Version Check---"
-if [ -z "$CUR_V" ]; then
-	echo "---Firefox not installed, installing---"
-	cd ${DATA_DIR}
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/Firefox-$LAT_V-$FIREFOX_LANG.tar.bz2 "https://download.mozilla.org/?product=firefox-${FIREFOX_V}&os=linux64&lang=${FIREFOX_LANG}" ; then
-		echo "---Sucessfully downloaded Firefox---"
+echo "---Checking for old logfiles---"
+find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
+find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
+echo "---Checking for old display lock files---"
+rm -rf /tmp/.X99*
+rm -rf /tmp/.X11*
+rm -rf ${DATA_DIR}/.vnc/*.log ${DATA_DIR}/.vnc/*.pid
+chmod -R ${DATA_PERM} ${DATA_DIR}
+if [ -f ${DATA_DIR}/.vnc/passwd ]; then
+	if [ "${RUNASROOT}" == "true" ]; then
+		chmod 600 /root/.vnc/passwd
 	else
-		echo "---Something went wrong, can't download Firefox, putting container in sleep mode---"
-		sleep infinity
+		chmod 600 ${DATA_DIR}/.vnc/passwd
 	fi
-	tar -C ${DATA_DIR} --strip-components=1 -xf ${DATA_DIR}/Firefox-$LAT_V-$FIREFOX_LANG.tar.bz2
-	rm -R ${DATA_DIR}/Firefox-$LAT_V-$FIREFOX_LANG.tar.bz2
-elif [ "$CUR_V" != "$LAT_V" ]; then
-	echo "---Version missmatch, installed v$CUR_V, downloading and installing latest v$LAT_V...---"
-    cd ${DATA_DIR}
-	find . -maxdepth 1 ! -name profile -exec rm -r {} \; 2>/dev/null
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/Firefox-$LAT_V-$FIREFOX_LANG.tar.bz2 "https://download.mozilla.org/?product=firefox-${FIREFOX_V}&os=linux64&lang=${FIREFOX_LANG}" ; then
-		echo "---Sucessfully downloaded Firefox---"
-	else
-		echo "---Something went wrong, can't download Firefox, putting container in sleep mode---"
-		sleep infinity
-	fi
-	tar -C ${DATA_DIR} --strip-components=1 -xf ${DATA_DIR}/Firefox-$LAT_V-$FIREFOX_LANG.tar.bz2
-	rm -R ${DATA_DIR}/Firefox-$LAT_V-$FIREFOX_LANG.tar.bz2
-elif [ "$CUR_V" == "$LAT_V" ]; then
-	echo "---Firefox v$CUR_V up-to-date---"
 fi
+screen -wipe 2&>/dev/null
 
-echo "---Preparing Server---"
-if [ ! -d ${DATA_DIR}/profile ]; then
-	mkdir -p ${DATA_DIR}/profile
-fi
-if [ ! -d ${DATA_DIR}/defaults/pref ]; then
-	mkdir -p ${DATA_DIR}/defaults/pref
-fi
-if [ ! -f ${DATA_DIR}/defaults/pref/autoconfig.js ]; then
-	cp /tmp/config/autoconfig.js ${DATA_DIR}/defaults/pref/autoconfig.js
-fi
-if [ ! -f ${DATA_DIR}/mozilla.cfg ]; then
-	cp /tmp/config/mozilla.cfg ${DATA_DIR}/mozilla.cfg
-else
-	if [ ! -z "$(grep "unraid" ${DATA_DIR}/mozilla.cfg)" ]; then
-		rm ${DATA_DIR}/mozilla.cfg
-		cp /tmp/config/mozilla.cfg ${DATA_DIR}/mozilla.cfg
-	fi
-fi
 echo "---Resolution check---"
 if [ -z "${CUSTOM_RES_W} ]; then
 	CUSTOM_RES_W=1024
@@ -81,18 +38,6 @@ if [ "${CUSTOM_RES_H}" -le 767 ]; then
 	echo "---Height to low must be a minimal of 768 pixels, correcting to 768...---"
     CUSTOM_RES_H=768
 fi
-echo "---Checking for old logfiles---"
-find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
-find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
-echo "---Checking for old display lock files---"
-rm -rf /tmp/.X99*
-rm -rf /tmp/.X11*
-rm -rf ${DATA_DIR}/.vnc/*.log ${DATA_DIR}/.vnc/*.pid
-chmod -R ${DATA_PERM} ${DATA_DIR}
-if [ -f ${DATA_DIR}/.vnc/passwd ]; then
-	chmod 600 ${DATA_DIR}/.vnc/passwd
-fi
-screen -wipe 2&>/dev/null
 
 echo "---Starting TurboVNC server---"
 vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :99 -rfbport ${RFB_PORT} -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
@@ -104,6 +49,7 @@ echo "---Starting noVNC server---"
 websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem ${NOVNC_PORT} localhost:${RFB_PORT}
 sleep 2
 
-echo "---Starting Firefox---"
+
+echo "---Starting JDupes-GUI ---"
 cd ${DATA_DIR}
-${DATA_DIR}/firefox --display=:99 --profile ${DATA_DIR}/profile --P ${USER} --setDefaultBrowser ${EXTRA_PARAMETERS}
+./jdupes-gui
